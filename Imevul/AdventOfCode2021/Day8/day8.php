e4b5ae3f4b7267da8859eb509566de992afe584b
Jonas Söderberg <imevul@gmail.com>
2021-12-07 07:18:12 +0100

    + Added boilerplate for Day8

---
+++ Imevul/AdventOfCode2021/Day8/day8.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * Boilerplate
+ * URL
+ */
+
+namespace Imevul\AdventOfCode2021\Day8;
+
+require_once __DIR__ . '/../../../bootstrap.php';
+
+function getConvertedInput(array $input): array {
+	return $input;
+}
+
+/**
+ * @param array $input The list of input
+ * @return int The result
+ */
+function part1(array $input): int {
+	$result = 0;
+
+	foreach ($input as $item) {
+		$result += (int)$item;
+	}
+
+	return $result;
+}
+
+/**
+ * @param array $input The list of input
+ * @return int The result
+ */
+function part2(array $input): int {
+	return 0;
+}
+
+return [test([NULL, NULL]), run(empty($skipRun))];

314ade19e141a7301076917dc1ec684b0c16d4f9
Jonas Söderberg <imevul@gmail.com>
2021-12-07 07:06:08 +0100

    + Added Day7 solution + Added timing code

--- Imevul/AdventOfCode2021/Day7/day7.php
+++ Imevul/AdventOfCode2021/Day7/day7.php
@@ -1,7 +1,7 @@
 <?php
 /*
- * Boilerplate
- * URL
+ * The Treachery of Whales
+ * https://adventofcode.com/2021/day/7
  */
 
 namespace Imevul\AdventOfCode2021\Day7;
@@ -9,29 +9,34 @@ namespace Imevul\AdventOfCode2021\Day7;
 require_once __DIR__ . '/../../../bootstrap.php';
 
 function getConvertedInput(array $input): array {
-	return $input;
+	return array_map('intval', explode(',', $input[0]));
 }
 
 /**
- * @param array $input The list of input
+ * Get the cost for all crabs to move to a specific position
+ * @param int $position Target position
+ * @param array<int> $crabs Array of crabs
+ * @param bool $incRate True to use incremental fuel cost
+ * @return int
+ */
+function getCost(int $position, array $crabs, bool $incRate = FALSE): int {
+	return array_sum(array_map(fn($c) => $incRate ? (abs($position - $c) * (abs($position - $c) + 1) / 2) : abs($position - $c), $crabs));
+}
+
+/**
+ * @param array<int> $input The list of input
  * @return int The result
  */
 function part1(array $input): int {
-	$result = 0;
-
-	foreach ($input as $item) {
-		$result += (int)$item;
-	}
-
-	return $result;
+	return array_reduce(range(min(...$input), max(...$input)), fn($c, $v) => min($c, getCost($v, $input)), INF);
 }
 
 /**
- * @param array $input The list of input
+ * @param array $input<int> The list of input
  * @return int The result
  */
 function part2(array $input): int {
-	return 0;
+	return array_reduce(range(min(...$input), max(...$input)), fn($c, $v) => min($c, getCost($v, $input, TRUE)), INF);
 }
 
-return [test([NULL, NULL]), run(empty($skipRun))];
+return [test([37, 168]), run(empty($skipRun))];
--- Imevul/AdventOfCode2021/index.php
+++ Imevul/AdventOfCode2021/index.php
@@ -4,6 +4,8 @@ namespace Imevul\AdventOfCode2021;
 
 use Console_Color2;
 use Console_Table;
+use DateTime;
+use SebastianBergmann\Timer\Timer;
 
 require_once __DIR__ . '/../../bootstrap.php';
 
@@ -19,7 +21,12 @@ for ($i = 1; $i <= $maxDay; $i++) {
 	$skipRun = (ONLY_LATEST && $i === $maxDay) || !ONLY_LATEST;
 
 	if (file_exists($filename)) {
-		$days[$i] = [$i, ...include_once($filename)];
+		$timer = new Timer;
+		$timer->start();
+		$result = include_once($filename);
+		$duration = $timer->stop();
+		$days[$i] = [$i, ...$result, $duration->asString()];
+
 	}
 }
 
@@ -49,14 +56,16 @@ foreach ($days as $day) {
 		$day[1][0] && $day[1][1] ? success($day[0], IS_CLI) : failure($day[0], IS_CLI),
 		$day[1][0] ? success($day[2][0], IS_CLI) : failure($day[2][0], IS_CLI),
 		$day[1][1] ? success($day[2][1], IS_CLI) : failure($day[2][1], IS_CLI),
+		$day[3],
 	];
 }
 
 $table = new Console_Table(color: IS_CLI);
-$table->setHeaders(['Day', 'Part1', 'Part2']);
+$table->setHeaders(['Day', 'Part1', 'Part2', 'Time']);
 $table->addData($data);
 $tableText = $table->getTable();
 $tableText = IS_CLI ? $tableText : str_replace([' ', PHP_EOL], ['&nbsp;', '<br>'], $tableText);
 
+echo (new DateTime('now'))->format('c') . PHP_EOL;
 echo $tableText;
 

5695eb376cbf7389ec7270a1b01f2acef4ca3d78
Jonas Söderberg <imevul@gmail.com>
2021-12-10 12:08:58 +0100

    * Clean up Day10

--- Imevul/AdventOfCode2021/Day10/day10.php
+++ Imevul/AdventOfCode2021/Day10/day10.php
@@ -6,103 +6,55 @@
 
 namespace Imevul\AdventOfCode2021\Day10;
 
-use ParseError;
-
 require_once __DIR__ . '/../../../bootstrap.php';
 
 function getConvertedInput(array $input): array {
-	return $input;
+	return array_map(fn($i) => str_split($i), $input);
 }
 
 /**
- * Parse all chunks from a string definition. Return the chunks (in order), and any missing symbols needed to complete any incomplete chunks
- * @param string $def Chunk definition
- * @return array{array, array}
+ * Parse all chunks from a definition. Return the chunks (in order), and any missing symbols needed to complete any incomplete chunks
+ * @param array $def Chunk definition
+ * @return array{string, array} [Illegal char, missing closing symbols]
  */
-function parseChunks(string $def): array {
+function parseChunks(array $def): array {
 	$stack = [];
-	$chunks = [];
-	$pairs = [
-		'(' => ')',
-		'[' => ']',
-		'{' => '}',
-		'<' => '>',
-	];
+	$pairs = [ '(' => ')', '[' => ']', '{' => '}', '<' => '>' ];
 
-	foreach (str_split($def) as $i => $char) {
+	foreach ($def as $char) {
 		if (in_array($char, array_keys($pairs))) {
 			$stack[] = $char;
-			$chunks[] = $char;
 		} else {
-			$expected = $pairs[array_pop($stack)];
-
-			if ($char !== $expected) {
-				throw new ParseError(json_encode([$i, $expected, $char]));
+			if ($char !== $pairs[array_pop($stack)]) {
+				return [$char, []];
 			}
 		}
 	}
 
-	return [$chunks, array_reverse(array_map(fn($c) => $pairs[$c], $stack))];
+	return [NULL, array_map(fn($c) => $pairs[$c], array_reverse($stack))];
 }
 
 /**
- * @param array<string> $input The list of input
+ * @param array<array> $input The list of input
  * @return int The result
  */
 function part1(array $input): int {
-	$scoring = [
-		')' => 3,
-		']' => 57,
-		'}' => 1197,
-		'>' => 25137,
-	];
+	$scoring = [ ')' => 3, ']' => 57, '}' => 1197, '>' => 25137 ];
 
-	return array_sum(
-		array_map(
-			function($v) use ($scoring) {
-				try {
-					parseChunks($v);
-					return 0;
-				} catch (ParseError $e) {
-					return $scoring[json_decode($e->getMessage())[2]];
-				}
-			},
-			$input
-		)
-	);
+	return array_sum(array_map(fn($v) => $scoring[parseChunks($v)[0]] ?? 0, $input));
 }
 
 /**
- * @param array<string> $input The list of input
+ * @param array<array> $input The list of input
  * @return int The result
  */
 function part2(array $input): int {
-	$scoring = [
-		')' => 1,
-		']' => 2,
-		'}' => 3,
-		'>' => 4,
-	];
+	$scoring = [ ')' => 1, ']' => 2, '}' => 3, '>' => 4 ];
 
-	$lines = array_filter(
-		array_map(
-			function($v) use ($scoring) {
-				try {
-					$c = parseChunks($v);
-					return [
-						...$c,
-						array_reduce($c[1], fn($c, $v) => $c * 5 + $scoring[$v], 0)
-					];
-				} catch (ParseError) { return NULL; }
-			},
-			$input
-		),
-		fn($v) => $v !== NULL
-	);
+	$lines = array_filter(array_map(fn($v) => array_reduce(parseChunks($v)[1], fn($c, $v) => $c * 5 + $scoring[$v], 0), $input), fn($v) => $v !== 0);
+	sort($lines);
 
-	usort($lines, fn($l1, $l2) => compare($l1[2], $l2[2]));
-
-	return $lines[floor(count($lines) / 2)][2];
+	return $lines[floor(count($lines) / 2)];
 }
 
 return [test([26397, 288957]), run(empty($skipRun))];
